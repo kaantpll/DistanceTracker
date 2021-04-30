87abf6a6917e5692b7a3490c929df6c2b227de6e
kaantpll <kaant43@gmail.com>
2021-04-30 13:14:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/distancetrackerapp/MapsFragment.kt
@@ -0,0 +1,52 @@
+package com.example.distancetrackerapp
+
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.distancetrackerapp.databinding.FragmentMapsBinding
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : Fragment(),OnMapReadyCallback  {
+
+    private var _binding : FragmentMapsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+       _binding = FragmentMapsBinding.inflate(inflater, container, false)
+
+        binding.startButton.setOnClickListener {  }
+        binding.stopButton.setOnClickListener {  }
+        binding.resetButton.setOnClickListener {  }
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/distancetrackerapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.distancetrackerapp.util
+
+object Constants {
+
+    const val PERMISSION_LOCATION_REQUEST_CODE =1
+}
